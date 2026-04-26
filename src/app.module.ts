69106c6d5c0340c38031e98a370954bd2352bae2
arthurair3s <arthuraires0@gmail.com>
2026-04-26 00:04:48 -0300

    ajusta app,module.ts e main.ts para aceitar novas entidades criadas anteriormente

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,19 @@ import { UsersModule } from './users/users.module';
 import { ProfilesModule } from './profiles/profiles.module';
 import { PermissionsModule } from './permissions/permissions.module';
 import { AuthModule } from './auth/auth.module';
+import { CharactersModule } from './characters/characters.module';
+import { CharacterVersionsModule } from './character-versions/character-versions.module';
+import { IslandCharacterVersionsModule } from './island-character-versions/island-character-versions.module';
 
 @Module({
   imports: [
+    AuthModule,
     UsersModule,
     ProfilesModule,
     PermissionsModule,
-    AuthModule,
+    CharactersModule,
+    CharacterVersionsModule,
+    IslandCharacterVersionsModule,
 
     ConfigModule.forRoot({ isGlobal: true }),
     SequelizeModule.forRootAsync({
--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,6 @@ async function bootstrap() {
     .setTitle('Grand Line API')
     .setDescription('Documentação da API')
     .setVersion('1.0')
-    .addTag('Base')
     .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' }, 'access-token')
     .build();
 
